d1c524840639a7a6c8f8b5ce91a6825bcac94c5c
ilfa neilna <162510291+IlfaNeilna@users.noreply.github.com>
2024-06-13 01:45:16 +0700

    Add files via upload

---
+++ tugasPWeb/Form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+      <h1>Login</h1>
+      <form action="login.php" method="POST">
+          <label for="username">Username:</label>
+          <input type="text" name="username" id="" placeholder="Username"><br><br>
+          <label for="password">Password:</label>
+          <input type="password" name="password" id="" placeholder="Password"><br><br>
+          <input type="Submit" value="Submit">
+      </form>
+  </body>
+</html>
